d1112b968eb703bedc2ee229c1d78d7ad563b6ae
Emil Telstad <22004178+emilte@users.noreply.github.com>
2023-12-03 01:56:30 +0100

    Add template.

---
+++ template/solution.py
@@ -0,0 +1,17 @@
+"""
+https://adventofcode.com/2022/day/<x>
+"""
+from task_input import task_input
+
+
+def part_1():
+    ...
+
+
+def part_2():
+    ...
+
+
+if __name__ == '__main__':
+    part_1()
+    part_2()
